e09c9a3c20a542b37ec8d678cc79bd3944b2bf50
TheDarkBatman <87569239+TheDarkBatman2@users.noreply.github.com>
2021-10-07 13:28:29 +0330

    Added Basic stuff
    -Sound manager/library
    -Fps manager

---
+++ Destruction Simulator/Assets/Scripts/Libraries/SoundLibrary.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class SoundLibrary : MonoBehaviour
+{
+    public AudioClip[] _sfxList;
+    public static Dictionary<string ,AudioClip> sfxLibrary = new Dictionary<string, AudioClip>();
+    
+    void Awake() {
+        _sfxList = Resources.LoadAll("SFX" , typeof(AudioClip)).Cast<AudioClip>().ToArray(); // load all of sfx in resources folder ( thats why this folder is called resources )
+        for (int i = 0; i < _sfxList.Length; i++){
+            sfxLibrary.Add(_sfxList[i].name ,_sfxList[i]);
+        }
+    }
+
+}
---
+++ Destruction Simulator/Assets/Scripts/Managers/SoundManager.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class SoundManager : MonoBehaviour 
+{
+    [SerializeField] private GameObject soundObject;
+    public void PlaySound(AudioClip soundClip, float volume = 0.4f, float variance = 0.2f) {
+        GameObject newSound = Instantiate(soundObject);
+        AudioSource soundSource = newSound.GetComponent<AudioSource>();
+
+        soundSource.volume = volume;
+        soundSource.pitch += Random.Range(-variance, variance);
+
+        soundSource.PlayOneShot(soundClip);
+
+        Destroy(newSound, soundClip.length);
+    }
+
+}
